b4c1492b768633815ef6e79a6e1dea5bbbc91aa0
unknown <mohnishram2001@gmail.com>
2021-09-19 19:04:03 +1200

    Finished main algorithm, still need to complete helper methods

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ c1 = 0
 c2 = 2
 D = 20
 
+Particles = []
+
 
 # Equations
-def rosenbrock(x):
+def rosenbrock(particle):
+    x = particle.pbest_pos
     return 100 * square_itself(((x * x) - x + 1)) + square_itself(x - 1)
 
 
@@ -21,12 +24,33 @@ def square_itself(x):
 
 
 class Particle:
-    def __init__(self, position, velocity):
-        self.position = position
-        self.velocity = velocity
+    def __init__(self):
+        self.position = []  # Need to change later
+        self.velocity = []  # Need to change later
+        self.pbest_val = float('inf')
+        self.pbest_pos = float('inf')
 
 
 def pso():
+    for i in range(Particle_size):
+        for dimension in range(D):
+            Particles.append(Particle)
+
+    k = 0
+    while k < Iterations:
+        for particle in Particles:
+            if rosenbrock(particle.pbest_pos) > particle.pbest_val:
+                particle.pbest_val = rosenbrock(particle.pbest_pos)
+
+        Particles.sort(key=lambda value: rosenbrock(value), reverse=True)
+        g_best = Particles[0].pbest_pos
+
+        for particle in Particles:
+            for dimension in range(D):
+                # Calculate velocity
+                r = 1
+                # Upadate Position
+        k = k + 1
     return 0
 
 
