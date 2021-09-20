01584deb84a5dafef2dd8e27d453507d1b14b538
unknown <mohnishram2001@gmail.com>
2021-09-20 18:45:56 +1200

    Fully debugged rosenbrooks minimisation

--- main.py
+++ main.py
@@ -3,22 +3,26 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-Iterations = 100
+Iterations = 1000
 Particle_size = 20
-c1 = 0.75  # Cognitive
-c2 = 1.4  # Social
+c1 = 1.496  # Cognitive
+c2 = 1.496  # Social
 D = 20  # Dimensions
+W = 0.7298
 
 g_best = []
 Particles = []
 
+fitness_global_initial = float("inf")
+
 
 # Equations
-def rosenbrock(particle):
-    x = particle.pbest_pos
+def rosenbrock(position):
+    x = position
     final = 0
     for d in range(D):
-        final += (100 * square_itself(((x[d] * x[d]) - x[d] + 1)) + square_itself(x[d] - 1))
+        final += (100 * square_itself(((x[d] ** 2) - x[d]))) + square_itself(x[d] - 1)
+
     return final
 
 
@@ -27,53 +31,59 @@ def square_itself(x):
     return x * x
 
 
-class Particle:
-    def __init__(self):
-        self.position = []  # Need to change later
-        self.velocity = []  # Need to change later
-        self.pbest_val = 0
-        self.pbest_pos = self.position
-
-
-def initilise_particle(particle):
+def initilise_position():
     pos = []
-    vel = []
     for dimension in range(D):
-        pos.append((random.random() * 30))
-        vel.append((random.random() * 30))
+        pos.append((random.randint(-30, 30)))
+    return pos
 
-    particle.position = pos
-    particle.velocity = vel
-    particle.pbest_pos = pos
-    particle.pbest_val = 0
+
+def initilise_velocity():
+    vels = []
+    for dimension in range(D):
+        vels.append(random.uniform(-1, 1))
+    return vels
+
+
+class Particle:
+    def __init__(self, position, velocity):
+        self.position = position
+        self.velocity = velocity
+        self.pbest_pos = self.position
 
 
 def pso():
     global g_best
+    global fitness_global_initial
+    g_best = initilise_position()
+
     for i in range(Particle_size):
         for dimension in range(D):
-            Particles.append(Particle)
+            Particles.append(Particle(initilise_position(), initilise_velocity()))
 
-    for particle in Particles:
-        initilise_particle(particle)
     k = 0
     while k < Iterations:
-        for particle in Particles:
-            for dimension in range(D):
-                if rosenbrock(particle) < particle.pbest_val:
-                    particle.pbest_val = rosenbrock(particle)
-
-        Particles.sort(key=lambda value: rosenbrock(value), reverse=True)
-        g_best = Particles[0].pbest_pos
 
         for particle in Particles:
-            for dimension in range(D):
-                vel = particle.velocity[dimension] + (c1 * random.random()) * (
-                        particle.pbest_pos[dimension] - particle.position[dimension]) + (random.random() * c2) * \
-                      (g_best[dimension] - particle.position[dimension])
+            if rosenbrock(particle.position) < rosenbrock(particle.pbest_pos):
+                particle.pbest_pos = np.copy(particle.position)
 
-                pos = vel + particle.position[dimension]
-                particle.position[dimension] = pos
+        Particles.sort(key=lambda value: rosenbrock(value.pbest_pos), reverse=False)
+        current_best = np.copy(Particles[0].pbest_pos)
+
+        if rosenbrock(current_best) < rosenbrock(g_best):
+            g_best = np.copy(current_best)
+
+        print("Iteration: " + str(k) + "Value: " + str(rosenbrock(g_best)))
+
+        for particle in Particles:
+            for d in range(D):
+                particle.velocity[d] = (W * particle.velocity[d]) + (c1 * random.random()) * \
+                                       (particle.pbest_pos[d] - particle.position[d]) + \
+                                       (c2 * random.random()) * (g_best[d] - particle.position[d])
+
+                particle.position[d] = particle.velocity[d] + particle.position[d]
+
         k = k + 1
     print(g_best)
 
